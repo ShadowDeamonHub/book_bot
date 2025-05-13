2211646ebbb9b48f87bb6710298a4ccab118660c
ShadowDeamonHub <therealaltruisticsith@gmail.com>
2025-05-13 18:55:20 -0400

    got scritp working

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-def get_book_text(filepath)
+def get_book_text(filepath):
+    with open(filepath) as f:
+        # f is a file object
+        file_contents = f.read()
+    return file_contents
+def main():
 
-main 
+    book_text = get_book_text("/home/patri/Boot_Dev_clourses/book_bot/books/frankenstein.txt")
+    print(book_text)
 
-    get_book_text(/home/patri/Boot_Dev_clourses/book_bot/books/franenstein.txt)
 
-Main()
+
+main()
