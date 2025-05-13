f2b7656167e5a246f3ef7ba6eeb05bce41e09258
ShadowDeamonHub <therealaltruisticsith@gmail.com>
2025-05-13 23:33:12 +0000

    Added Character counter

--- stats.py
+++ stats.py
@@ -7,3 +7,13 @@ def word_count(book_text):
     #counts the word in the book
     word_count = book_text.split()
     return len(word_count)
+def get_caracter_count(book_text):
+    #counts the character in the book
+    lowercase_charater = book_text.lower()
+    character_count = {}
+    for character in lowercase_charater:
+        if character in character_count:
+            character_count[character] += 1
+    else:
+        character_count[character] = 1
+    return len(character_count)
