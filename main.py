c816c3dd692e1c8ac9ee20df371c7f5e3f1d9979
ShadowDeamonHub <therealaltruisticsith@gmail.com>
2025-05-13 19:09:20 -0400

    Fixed main.py word count funtion

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ def get_book_text(filepath):
         # f is a file object
         file_contents = f.read()
     return file_contents
-def Word_count(book_text):
+def word_count(book_text):
     #counts the word in the book
     word_count = book_text.split()
     return len(word_count)
 def main():
 
     book_text = get_book_text("/home/patri/Boot_Dev_clourses/book_bot/books/frankenstein.txt")
-    print(book_text)
+    num_words = word_count(book_text)
+    print(f"{num_words} words found in the document")
 
 
 
