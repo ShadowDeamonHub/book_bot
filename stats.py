2f13982527250243b563f9aa52807595d677710d
ShadowDeamonHub <therealaltruisticsith@gmail.com>
2025-05-14 19:40:28 +0000

    fix #8 sorting test code

--- stats.py
+++ stats.py
@@ -21,6 +21,6 @@ def get_character_count(book_text):
 def dict_sorted(character_count, book_text):
     sort_key = list(set(book_text))
     ussorted_character_count = character_count
-    character_sorted = ussorted_character_count.sort(reversed= True, key = sort_key)
+    character_sorted = list(ussorted_character_count).sort(reversed= True, key = sort_key)
     return character_sorted
      
