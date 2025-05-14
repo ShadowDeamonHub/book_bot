939cc18ffd27ba3e99d33659acf7a627add936fe
ShadowDeamonHub <therealaltruisticsith@gmail.com>
2025-05-14 18:56:26 +0000

    fixed sorting test code

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ def main():
     from stats import get_book_text, word_count, get_character_count, dict_sorted
     book_text = get_book_text("/home/patri/Boot_Dev_clourses/book_bot/books/frankenstein.txt")
     num_words = word_count(book_text)
-    num_characters = dict_sorted(sorted_character_count)
+    num_characters =get_character_count(book_text)
+    sorted_Character_Count = dict_sorted(num_characters)
     print("============ BOOKBOT ============")
     print(f"Analyzing book found a books/frankenstein.txt...")
     print("----------- Word Count ----------")
     print(f"{num_words} words found in the document")
     print("----------- Character Count -----------")
-    print(num_charaters)
+    print(sorted_Character_Count)
+
 
 
 
