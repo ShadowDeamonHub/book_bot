d0aa468221d020b73599c2d55306d8cfd72d6c34
ShadowDeamonHub <therealaltruisticsith@gmail.com>
2025-05-14 21:25:34 +0000

    sys.argv command added

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 def main():
+    import sys
     from stats import get_book_text, word_count, get_character_count, sorting 
-    book_text = get_book_text("/home/patri/Boot_Dev_clourses/book_bot/books/frankenstein.txt")
+    #checks for sys.argv to have two arguments
+    if sys.argv[1] == None:
+        pirnt("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_text = get_book_text(sys.argv)
     num_words = word_count(book_text)
     num_characters =get_character_count(book_text)
     num_characters.sort(reverse=True, key=sorting)
